72fb1898a7171c485f861ee6edd42b1e37cc1c84
Manu160296 <fmanuela499@gmail.com>
2017-11-19 11:19:18 -0500

    version 1.0 finalizada

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 
+var addList= document.querySelector('.add-list-js');
+var container = document.querySelector('.container');
+console.log(container)
+console.log(addList)
+
+addList.addEventListener('click',showForm);
+
+function showForm(e) {
+  console.log(event.target)
+//  e.target.classList.remove('add-list');
+  //e.target.classList.add('hide');
+  //creando hijos: formulario , input , boton que agrega tareas:
+  var form=  document.createElement('form');
+  var input = document.createElement('input');
+  var addButton= document.createElement('button');
+  var cancelButton = document.createElement('button');
+  //reemplazando el addlist por el formulario :
+  container.replaceChild(form,addList);
+  //creando hijos:
+  form.appendChild(input);
+  form.appendChild(addButton);
+  form.appendChild(cancelButton);
+  //agregando atributos y detalles a los elementos creados:
+  input.setAttribute('placeholder','añadir una lista');
+  addButton.textContent = 'Añadir';
+  cancelButton.textContent='X';
+
+}
